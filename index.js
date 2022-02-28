073e97b1f5c40393e7b22e8e9e84fe3b5eda0b19
Sean <larcenciel12@gmail.com>
2022-02-28 22:31:53 +0800

    first version

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+
+
+
+const initServer = async () => {
+
+  app.use('/', express.static(__dirname));
+
+  app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+  console.log("init server")
+   
+    
+
+}
+
+
+
+
+
+initServer().catch(err => {
+    console.log(err);
+  })
+  
